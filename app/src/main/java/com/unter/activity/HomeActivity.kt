ccde6dbea7a2cf290eccb4a5ec7363bf82d4d861
samwalls <sam@s-w.io>
2019-02-15 16:43:25 +0000

    add basic nav with action links

---
+++ app/src/main/java/com/unter/activity/HomeActivity.kt
@@ -0,0 +1,14 @@
+package com.unter.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+import com.unter.R
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
