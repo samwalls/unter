1458cfd25136293dc083112a0c5f18361cc0b002
samwalls <sam@s-w.io>
2019-02-23 22:05:49 +0000

    add basic route calculation

--- app/src/main/java/com/unter/JourneyRequest.kt
+++ app/src/main/java/com/unter/JourneyRequest.kt
@@ -2,14 +2,19 @@ package com.unter
 
 import android.annotation.SuppressLint
 import android.arch.lifecycle.ViewModelProviders
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.util.Log.d
+import android.util.Log.e
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.mapbox.android.core.permissions.PermissionsListener
 import com.mapbox.android.core.permissions.PermissionsManager
+import com.mapbox.api.directions.v5.models.DirectionsResponse
+import com.mapbox.api.directions.v5.models.DirectionsRoute
+import com.mapbox.geojson.Point
 import com.mapbox.mapboxsdk.Mapbox
 import com.mapbox.mapboxsdk.camera.CameraPosition
 import com.mapbox.mapboxsdk.camera.CameraUpdateFactory
@@ -22,8 +27,15 @@ import com.mapbox.mapboxsdk.maps.MapboxMap
 import com.mapbox.mapboxsdk.maps.Style
 import com.mapbox.mapboxsdk.plugins.annotation.SymbolManager
 import com.mapbox.mapboxsdk.plugins.annotation.SymbolOptions
+import com.mapbox.mapboxsdk.style.layers.PropertyFactory.*
+import com.mapbox.mapboxsdk.style.layers.SymbolLayer
+import com.mapbox.mapboxsdk.style.sources.GeoJsonSource
+import com.mapbox.services.android.navigation.ui.v5.route.NavigationMapRoute
+import com.mapbox.services.android.navigation.v5.navigation.NavigationRoute
 import com.unter.model.UnterAppModel
-
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 
 /**
@@ -35,7 +47,10 @@ import com.unter.model.UnterAppModel
  * create an instance of this fragment.
  *
  */
+@SuppressLint("LogNotTimber")
 class JourneyRequest : Fragment(), PermissionsListener {
+
+    private val TAG: String = JourneyRequest::class.java.canonicalName!!
 
     private lateinit var model: UnterAppModel
 
@@ -44,6 +59,9 @@ class JourneyRequest : Fragment(), PermissionsListener {
 
     private lateinit var permissionsManager: PermissionsManager
     private lateinit var locationComponent: LocationComponent
+
+    private lateinit var routePickupToDestination: DirectionsRoute
+    private var navigationMapRoute: NavigationMapRoute? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -81,22 +99,53 @@ class JourneyRequest : Fragment(), PermissionsListener {
     }
 
     private fun onMapReady() {
-// get the map style from the unter mapbox style URL
+        // get the map style from the unter mapbox style URL
         mapboxMap.setStyle(Style.Builder().fromUrl(context!!.getString(R.string.mapbox_unter_style))) { style: Style ->
+
+            addDestinationMarkerLayer(mapboxMap.style!!)
 
             enableLocationComponent(mapboxMap.style!!)
             moveCameraToCurrentLocation()
 
-            // get a symbol manager to add markers as required
-            val symbolManager = SymbolManager(mapView, mapboxMap, style)
-
             mapboxMap.addOnMapClickListener { position ->
-                val symbol = symbolManager.create(SymbolOptions().withLatLng(position))
-                symbol.textField = "destination"
-                d(JourneyRequest::class.java.canonicalName, "map clicked at coordinates: $position")
-                false
+                onMapClick(position)
             }
         }
+    }
+
+    private fun onMapClick(clickPos: LatLng): Boolean {
+
+        @SuppressLint("MissingPermission")
+        val origin: Point = Point.fromLngLat(
+            mapboxMap.locationComponent.lastKnownLocation!!.longitude,
+            mapboxMap.locationComponent.lastKnownLocation!!.latitude
+        )
+
+        val destination = Point.fromLngLat(clickPos.longitude, clickPos.latitude)
+
+        getRoute(origin, destination)
+
+        d(TAG, "map clicked at coordinates: $clickPos")
+        return false
+    }
+
+    private fun addDestinationMarkerLayer(style: Style) {
+        val geoJsonSource = GeoJsonSource("destination-source-id")
+        style.addSource(geoJsonSource)
+
+        style.addImage(
+            "destination-icon-id",
+            BitmapFactory.decodeResource(resources, R.drawable.mapbox_marker_icon_default)
+        )
+
+        val symbolLayer = SymbolLayer("destination-symbol-layer-id", "destination-source-id")
+        symbolLayer.withProperties(
+            iconImage("destination-icon-id"),
+            iconAllowOverlap(true),
+            iconIgnorePlacement(true)
+        )
+
+        style.addLayer(symbolLayer)
     }
 
     private fun moveCameraToCurrentLocation() {
@@ -112,6 +161,48 @@ class JourneyRequest : Fragment(), PermissionsListener {
 
             mapboxMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition), 1000)
         }
+    }
+
+    private fun getRoute(origin: Point, destination: Point) {
+        NavigationRoute.builder(context)
+            .accessToken(Mapbox.getAccessToken()!!)
+            .origin(origin)
+            .destination(destination)
+            .build()
+            .getRoute(object: Callback<DirectionsResponse> {
+                override fun onResponse(call: Call<DirectionsResponse>, response: Response<DirectionsResponse>) {
+                    onDirectionsResponse(call, response)
+                }
+
+                override fun onFailure(call: Call<DirectionsResponse>, t: Throwable) {
+                    onDirectionsFailure(call, t)
+                }
+            })
+    }
+
+    private fun onDirectionsResponse(call: Call<DirectionsResponse>, response: Response<DirectionsResponse>) {
+        d(TAG, "response code: ${response.code()}")
+        if (response.body() == null) {
+
+        } else if (response.body()!!.routes().size < 1) {
+            e(TAG, "no routes found (make sure to set the right user token and access token)")
+            return
+        }
+
+        // set the route on screen
+
+        routePickupToDestination = response.body()!!.routes()[0]
+        if (navigationMapRoute != null) {
+            // TODO change this deprecated code
+            navigationMapRoute!!.removeRoute()
+        } else {
+            navigationMapRoute = NavigationMapRoute(null, mapView, mapboxMap)
+        }
+        navigationMapRoute!!.addRoute(routePickupToDestination)
+    }
+
+    private fun onDirectionsFailure(call: Call<DirectionsResponse>, throwable: Throwable) {
+        e(TAG, "error: ${throwable.message}")
     }
 
     @SuppressWarnings("MissingPermission")
